86bd90c2771c521a6cb00c525e93ec8a776164ac
kingsdev101 <brianhiddenfeelings@gmail.com>
2021-02-04 04:37:40 +0000

    initial import

--- resources/views/official/projects.blade.php
+++ resources/views/official/projects.blade.php
@@ -5,33 +5,38 @@
 <div class="projects-container">
     <h2>Projects</h1>
         <div class="list-projects">
-            @foreach($project as $projects)
+        @if($project->count() >0)
+        @foreach($project as $projects)
 
-            <div class="projectac">
-                <h5>{{$projects->name}}</h5>
+<div class="projectac">
+    <h5>{{$projects->name}}</h5>
 
-                <ul>
-                    <li> <span>Type:</span>{{$projects->type}}</li>
-                    <li>
-                        <span>Year</span> {{$projects->year}}
+    <ul>
+        <li> <span>Type:</span>{{$projects->type}}</li>
+        <li>
+            <span>Year</span> {{$projects->year}}
 
-                    </li>
-                    <li>
+        </li>
+        <li>
 
-                        <span>what's here:</span>{{$projects->features}}
-                    </li>
-                    <li>
-                        <span>Language:</span>{{$projects->language}}
-                    </li>
-                    @if ($projects->name == 'Understanding God')
-                    <a href="{{$projects->links}}"> Visit</a>
-                    @else
-                    <a href="{{$projects->links}}"> Download</a>
-                    @endif
-                </ul>
+            <span>what's here:</span>{{$projects->features}}
+        </li>
+        <li>
+            <span>Language:</span>{{$projects->language}}
+        </li>
+        @if ($projects->name == 'Understanding God')
+        <a href="{{$projects->links}}"> Visit</a>
+        @else
+        <a href="{{$projects->links}}"> Download</a>
+        @endif
+    </ul>
 
-            </div>
-            @endforeach
+</div>
+@endforeach
+    @else
+    <div style="padding:20px;text-align:center">
+    <h1>No Record</h1></div>
+    @endif
 
         </div>
 
