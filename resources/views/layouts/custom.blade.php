c74e049dc6efc0f8085704273d0f1e472d5d29f3
kingsdev101 <brianhiddenfeelings@gmail.com>
2021-02-04 12:39:14 +0000

    theme

--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="black">
+	<meta name="msapplication-navbutton-color" content="black">
 
     <!-- Fonts -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
