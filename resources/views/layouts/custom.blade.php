63181aca0d74803f1af92b96e5924fac317a2c79
kingsdev101 <brianhiddenfeelings@gmail.com>
2021-02-05 05:18:25 +0000

    them br

--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#f75901">
-	<meta name="msapplication-navbutton-color" content="#f75901">
+    <meta name="theme-color" content="black">
+	<meta name="msapplication-navbutton-color" content="black">
 
     <script src="{{ asset('js/jquery-3.5.1.min.js') }} "></script>
 
